c3d3acbb3459ca3707e2631a0878dab211088f56
quanchiit <quanchi.it@gmail.com>
2025-06-25 10:58:35 +0700

    Add files via upload

---
+++ wp-config.php
@@ -0,0 +1,25 @@
+<?php
+
+define( 'DB_NAME', 'bebehapp6856_bb' );
+define( 'DB_USER', 'bebehapp6856_bb' );
+define( 'DB_PASSWORD', 'Bebehappy@#25' );
+define( 'DB_HOST', 'localhost:3306' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define('AUTH_KEY', '&)_24QGjU&*(45g-Jl26C2!xoJVy5C6#q!0FU:[/nrQj6P4]DS[LDI7F~XrO8O9t');
+define('SECURE_AUTH_KEY', '/510OOb]a9veBQ5i]~ib9Wr;RrS+Az+qmp;e+(8l5Rw6DYvU;W9q5wk:hTAbA:77');
+define('LOGGED_IN_KEY', 'v08Kk8|4Wpg*I35Hw753s|cYS4E0]D@2(K9tT4j400(Cx+Bs)_704s2sZ%n+1Vs5');
+define('NONCE_KEY', 'kO+T:1-h88_45!ma25]1gpcexZ1E30_np@S8~8870]uY3aw:5:[]YOeIOO-d#]6D');
+define('AUTH_SALT', ':4pd6ZYD/M4]EH;8M7*3[K90122!acC-fhjl4S~YlWQgG]4f;#Q35oDC%o9AAnl1');
+define('SECURE_AUTH_SALT', '7m2rK]Xo-F|*X9ep)|~7JsWtO:y/S93)A_x8azY29yOVpH9smG*9GF;|ce4|2:5D');
+define('LOGGED_IN_SALT', '~Rq:Eo3@3%ly+T~Z1M2139:_:L4+o949qk6l!1Qpyu;oo|m7a0v7:0u(25o+xbIo');
+define('NONCE_SALT', '2P:vbT*@m)2|@V###aPn_jr0cO_j)by8!7E2fvO#KVb8%b&Ed81#|EW:YnY!m@5A');
+$table_prefix = 'cgc2h8_';
+define('WP_ALLOW_MULTISITE', true);
+define( 'DISABLE_WP_CRON', true );
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'CONCATENATE_SCRIPTS', false );
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+require_once ABSPATH . 'wp-settings.php';
